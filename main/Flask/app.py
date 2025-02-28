8881c88671dc052e63678a2651f686550026079e
Areeba Shakeel <shakeelareeba02@gmail.com>
2025-02-28 12:14:34 +0530

    ..

---
+++ main/Flask/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) # create an instance of the Flask class
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__': # run the app
+    app.run()
